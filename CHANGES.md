83411983c9176bb21c6d0ca8d9d98d535fa8df10
Alexander Bias <alexander.bias@lernlink.de>
2024-01-15 15:30:55 +0100

    Prepare compatibility for Moodle 4.3.

--- CHANGES.md
+++ CHANGES.md
@@ -4,6 +4,10 @@ moodle-tool_directsso
 Changes
 -------
 
+### Unreleased
+
+* 2024-01-15 - Prepare compatibility for Moodle 4.3.
+
 ### v4.2-r1
 
 * 2024-01-15 - Prepare compatibility for Moodle 4.2.
--- version.php
+++ version.php
@@ -27,6 +27,6 @@
 $plugin->component = 'tool_directsso';
 $plugin->version = 2023042400;
 $plugin->release = 'v4.2-r1';
-$plugin->requires = 2023042400;
-$plugin->supported = [402, 402];
+$plugin->requires = 2023100900;
+$plugin->supported = [403, 403];
 $plugin->maturity = MATURITY_STABLE;
