334f9b6323f8fc861b0e15ed7759bfb0a27ca6b3
Alexander Bias <alexander.bias@lernlink.de>
2024-01-15 15:29:30 +0100

    Release v4.2-r1

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
  defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tool_directsso';
-$plugin->version = 2022112800;
-$plugin->release = 'v4.1-r1';
+$plugin->version = 2023042400;
+$plugin->release = 'v4.2-r1';
 $plugin->requires = 2023042400;
 $plugin->supported = [402, 402];
 $plugin->maturity = MATURITY_STABLE;
